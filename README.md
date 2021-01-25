ec9e13f9fc6991849a363501cd888b8d079677ec
Justin M <thisisjustinm@outlook.com>
2021-01-25 07:29:52 +0530

    Face for homepage

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 ## HTTP Status Birbs
-Hypertext Transfer Protocol Response status codes. With cute birbs. Chirp!
+>Hypertext Transfer Protocol Response status codes. With cute birbs. Chirp!
 
 ### Example
-![403](images/404.jpg)
+![418](images/418.jpg)
 
-### Misc.
+### Miscellaneous
 Images used here are not my property and are taken from various sites around the internet.
 Please note that the images are NOT used for monetary purposes in any way, shape, or form and copyright infringement is NOT intended.
-If you own a particular image and want it to be taken down, please open an [issue](https://github.com/thisisjustinm/http-status-birbs/issues) and I will take I down ASAP.
+If you own a particular image and want it to be taken down, please open an [issue](https://github.com/thisisjustinm/http-status-birbs/issues) and I will take it down ASAP.
 
 
-And yes, this is the birb version of [httpstatusdogs](https://httpstatusdogs.com/) and [http.cat](https://http.cat/)
+And yes, this is the birb version of [httpstatusdogs](https://httpstatusdogs.com/) and [http.cat](https://http.cat/).
--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import send_file, Flask
-import glob, os
+import glob
+import os
+from flask import send_file, Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates')
+files = glob.glob("images/*.jpg")
 
 
 @app.route('/')
-def home():
-    return 'Home of HTTP Status Birbs. Add /status_code to access.'
+def index():
+    code_list = [i[7:10] for i in files]
+    return render_template('index.html', code_list=code_list, b_count=len(code_list))
 
 
 @app.route('/<status>')
 def get_image(status):
-    files = glob.glob("images/*.jpg")
-    file_name = f'images/{status}.jpg'
-    print(files, file_name)
+    # file_name = f'images/{status}.jpg'
+    file_name = f'images\\{status}.jpg'
     if file_name in files:
         return send_file(file_name, mimetype='image/jpeg')
     else:
@@ -23,4 +25,5 @@ def get_image(status):
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port)
+    app.run(port=port, debug=True)
